36d30cbbf59ea40089270f27e744aa872d6b0886
Athang <113341199+Athang17@users.noreply.github.com>
2024-09-07 18:02:06 +0530

    Added L2 regularization

--- models.py
+++ models.py
@@ -1,31 +1,32 @@
 from tensorflow.keras.models import Sequential 
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.regularizers import l2
 
 def create_model():
     model = Sequential();
 
     #First layer
-    model.add(Conv2D(32,(3,3),activation='relu',input_shape=(48,48,1)))
+    model.add(Conv2D(32,(3,3),activation='relu',input_shape=(48,48,1),kernel_regularizer=l2(0.05)))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
     #Second layer
-    model.add(Conv2D(64,(3,3),activation='relu'))
+    model.add(Conv2D(64,(3,3),activation='relu',kernel_regularizer=l2(0.05)))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
     #Third layer
-    model.add(Conv2D(128,(3,3),activation='relu'))
+    model.add(Conv2D(128,(3,3),activation='relu',kernel_regularizer=l2(0.05)))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
     #Fourth layer
-    model.add(Conv2D(256,(3,3),activation='relu'))
+    model.add(Conv2D(256,(3,3),activation='relu',kernel_regularizer=l2(0.05)))
     model.add(MaxPooling2D(pool_size=(2,2)))
 
     #Flattening layer
     model.add(Flatten())
 
     #Fully Connected Dense Layer
-    model.add(Dense(512,activation='relu'))
+    model.add(Dense(512,activation='relu',kernel_regularizer=l2(0.01)))
     model.add(Dropout(0.1))
 
     #Output
